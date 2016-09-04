fb8b0d64a0ac4c05f2917737a70fbe756aff6745
ijavaiweb <2434501413@qq.com>
2016-09-04 14:29:28 +0800

    jianli

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('#page1down').click(function(){
+		$('#page2').css('display','block').siblings('.box').css('display','none')
+	})
+	$('#page2down').click(function(){
+		$('#page3').css('display','block').siblings('.box').css('display','none')
+	})
+	$('#page3down').click(function(){
+		$('#page4').css('display','block').siblings('.box').css('display','none')
+	})
+	$('#page4down').click(function(){
+		$('#page5').css('display','block').siblings('.box').css('display','none')
+	})
+	$('#page5down').click(function(){
+		$('#page1').css('display','block').siblings('.box').css('display','none')
+	})
+})
